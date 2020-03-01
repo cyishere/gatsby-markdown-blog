e277668ca8d4b77cfd5be1d5fc98417adc6e703d
cyishere <cyishere@gmail.com>
2020-03-01 17:22:17 +0800

    Resolved the can-not-build issue caused by UIkit

--- README.md
+++ README.md
@@ -10,7 +10,31 @@
 - [x] Display all tags in one page;
 - [x] Posts by tag;
 - [x] Posts by author;
-- [ ] Pagination;
+- [ ] Pagination: [Display Paginated Posts](https://egghead.io/lessons/react-display-paginated-posts-using-gatsby);
 - [ ] SEO: [https://www.gatsbyjs.org/docs/add-seo-component/](https://www.gatsbyjs.org/docs/add-seo-component/);
+- [x] <code>build</code>时由于<code>UIkit</code>会报错，需解决；
 
 I grabbed some articles from [The A.V. Club](https://www.avclub.com/) for dummy content.
+
+## Notes
+
+Because of using the third party js - UIkit, it throw error when build. The solution is changing the default arrow function in <code>layout.js</code> to a React component class, and adding <code>componentDidMount</code> function in it.
+
+```js
+class Layout extends React.Component {
+    componentDidMount() {
+        try {
+            this.UIkit = require('uikit');
+            this.Icons = require('uikit/dist/js/uikit-icons');
+            this.UIkit.use(this.Icons);
+        } catch(error) {
+            console.log(error);
+        }
+    }
+    render() {
+        return(
+            ......
+        )
+    }
+}
+```
--- src/templates/layout.js
+++ src/templates/layout.js
@@ -4,22 +4,49 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 import '../scss/main.scss';
 
-import UIkit from 'uikit';
-import Icons from 'uikit/dist/js/uikit-icons';
+// import UIkit from 'uikit';
+// import Icons from 'uikit/dist/js/uikit-icons';
 
-UIkit.use(Icons);
+// UIkit.use(Icons);
 
-const Layout = ({ children }) => {
-    return (
-        <div className="uk-width-1-1">
-            <Header />
 
-            {children}
+// const Layout = ({ children }) => {
+//     // componentDidMount() {
+//     //     UIkit.use(Icons);
+//     // }
 
-            <Footer />
-        </div>
-    );
-};
+//     return (
+//         <div className="uk-width-1-1">
+//             <Header />
+
+//             {children}
+
+//             <Footer />
+//         </div>
+//     );
+// };
+
+class Layout extends React.Component {
+    componentDidMount() {
+        try {
+            this.UIkit = require('uikit');
+            this.Icons = require('uikit/dist/js/uikit-icons');
+            this.UIkit.use(this.Icons);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    render() {
+        return(
+            <div className="uk-width-1-1">
+                <Header />
+                {this.props.children}
+                <Footer />
+            </div>
+        )
+    }
+}
 
 
 export default Layout;
